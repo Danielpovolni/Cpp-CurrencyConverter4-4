fcedbee18bf492d38628f6c58086e235d936366f
Danielp <64538910+Danielpovolni@users.noreply.github.com>
2021-05-09 21:00:26 +0200

    Update code.cpp
    Removed bug.

--- code.cpp
+++ code.cpp
@@ -1,22 +1,23 @@
-
 #include <iostream>
 #include <string>
 
 int main(){
+    for(int i = 0; i <= 1; i++){
 	using namespace std;
 	bool nice;
 	string currency_from, currency_to;
 	float amount, base;
 	cout << "Enter from witch currency you wanna convert(euro,yen,pound,franc,dollar): ";
 	cin >> currency_from;
-	if(currency_from != "dollar" && "euro" && "yen" && "franc"){
+	if(currency_from != "dollar" && currency_from != "euro" && currency_from != "yen" && currency_from != "franc"){
     cout << "Wrong input! " << endl;
-    nice = true;}
+    i = i - 1;}
 	else {
 	cout << "Enter to witch currency you wanna convert(euro,yen,pound,franc,dollar): ";
 	cin >> currency_to;
-	if(currency_to != "dollar" && "euro" && "yen" && "franc"){
-    cout << "Wrong input! " << endl;}
+	if(currency_to != "dollar" && currency_to != "euro" && currency_to != "yen" && currency_to != "franc"){
+    cout << "Wrong input! " << endl;
+    i=i-1;}
 	else{
 	cout << "Enter the amount you wanna convert: ";
 	cin >> amount;
@@ -24,6 +25,7 @@ int main(){
 	if(amount == 0){
         cout << "Wrong input!" << endl;
         nice = true;
+        i=i-1;
 	}
     nice == true;
 	}
@@ -55,4 +57,5 @@ int main(){
 	else if(currency_to == "franc"){
 		result= float_base*0.91; }
 	if(nice == false){
-	cout << amount<< " " <<  currency_from << "(s)" << " is " << result << " " <<  currency_to << "(s)" ;}}
+	cout << amount<< " " <<  currency_from << "(s)" << " is " << result << " " <<  currency_to << "(s)" ;}}}
+
